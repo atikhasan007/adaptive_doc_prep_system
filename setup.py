03e38f9e39421eabeba51e32dc8dc6095b7dc67b
Md Atik Hasan <atikhasan007@gmail.com>
2026-05-19 14:44:28 +0600

    update setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'Generative AI Project',
+    name = 'adaptive doc prep system',
     version = '0.0.0',
     author = 'Md atik hasan',
     author_email="imatik513@gmail.com",
