8e51829467d8fe55e5d39a4d18294594cc788d11
Md Atik Hasan <atikhasan007@gmail.com>
2026-05-20 10:06:02 +0600

    config seutp

--- demo.py
+++ demo.py
@@ -1,8 +1,34 @@
 
-# logger system check 
+
+
+# # logger system check 
+
+# from src.utils.logger import get_logger
+
+# logger = get_logger("pdf_processing")
+
+# logger.info("PDF loading started")
+
+
+
+
 
 from src.utils.logger import get_logger
+from src.config.config import get_config
 
-logger = get_logger("pdf_processing")
+# ================= LOGGER =================
+logger = get_logger("demo")
 
-logger.info("PDF loading started")
+logger.info("System starting...")
+
+# ================= CONFIG =================
+config = get_config()
+
+logger.info("Config Loaded Successfully")
+
+logger.info(f"PDF Path: {config['PDF_PATH']}")
+logger.info(f"DB Name: {config['MONGODB_DB']}")
+logger.info(f"Chroma Path: {config['CHROMA_PERSIST']}")
+logger.info(f"MCQ per section: {config['MCQ_PER_SECTION']}")
+
+logger.info("Demo setup completed successfully")
---
+++ src/config/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_config():
+    return {
+        "GEMINI_API_KEY": os.getenv("GEMINI_API_KEY"),
+        "MONGODB_URI": os.getenv("MONGODB_URI"),
+        "MONGODB_DB": os.getenv("MONGODB_DB", "adaptive_prep"),
+        "CHROMA_PERSIST": os.getenv("CHROMA_PERSIST_DIR", "./chroma_db"),
+        "PDF_PATH": os.getenv("PDF_PATH"),
+        "MCQ_PER_SECTION": int(os.getenv("MCQ_PER_SECTION", 5))
+    }
