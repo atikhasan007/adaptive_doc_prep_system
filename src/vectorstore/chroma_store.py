7a0e092948be25fb2182c31184534e06592c299b
Md Atik Hasan <atikhasan007@gmail.com>
2026-05-20 11:26:33 +0600

    embeded file and vector store added

---
+++ src/vectorstore/chroma_store.py
@@ -0,0 +1,18 @@
+from langchain_chroma import Chroma
+from src.embedding.embeddings import get_embeddings
+
+def create_vectorstore(all_chunks, persist_dir):
+
+    print(" Embedding chunks and storing in ChromaDB...")
+
+    vectorstore = Chroma.from_documents(
+        documents=all_chunks,
+        embedding=get_embeddings(),
+        persist_directory=persist_dir,
+        collection_name="slatefall_kb"
+    )
+
+    print(" ChromaDB ready")
+    print(" Total vectors:", vectorstore._collection.count())
+
+    return vectorstore
